728e97a623454497d0b671ad3960baaf42dcbd16
mlcsec <47215311+mlcsec@users.noreply.github.com>
2022-02-08 18:57:14 +0000

    Create Config.cs

---
+++ SharpSQL/Config.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SharpSQL
+{
+    class Config
+    {
+        public static string instance = "";
+        public static string ip = "";
+        public static string db = "master";
+        public static string linkedinstance = "";
+        public static string user = "";
+        public static string command = "whoami";
+        public static string query = "";
+    }
+}
